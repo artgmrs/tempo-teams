fd607ecd92038ce2ccf1f4944090e401098bcee5
Arthur Guimarães <C0639646@vale.com>
2022-05-23 19:08:56 -0300

    TeamDetails tests NoMatch route created Correcting handlers

--- src/shared/mocks/handlers.ts
+++ src/shared/mocks/handlers.ts
@@ -1,25 +1,79 @@
 import { rest } from "msw";
 
-const mockEndpoint = "https://cgjresszgg.execute-api.eu-west-1.amazonaws.com";
+const mockBaseUrl = "https://cgjresszgg.execute-api.eu-west-1.amazonaws.com";
 
 const mockTeamsReponse = [
-  { id: "1", name: "First User" },
-  { id: "2", name: "Second User" },
-  { id: "3", name: "Third User" },
-  { id: "4", name: "Fourth User" },
+  { id: "1", name: "Ordinary Coral Lynx" },
+  { id: "2", name: "Weekly Peach Wildebeest" },
+  { id: "3", name: "Surrounding Gold Pheasant" },
+  { id: "4", name: "Resident Scarlet Hare" },
 ];
 
-const mockUserResponse = {
+const mockUserResponseIdOne = {
   id: "1",
-  displayName: "user",
+  firstName: "Gianni",
+  lastName: "Wehner",
+  displayName: "gianniWehner",
+  avatarUrl: "https://cdn.fakercloud.com/avatars/rude_128.jpg",
+  location: "Brakusstad",
+};
+
+const mockUserResponseIdTwo = {
+  id: "2",
+  firstName: "Jaren",
+  lastName: "Kerluke",
+  displayName: "jarenKerluke",
+  avatarUrl: "https://cdn.fakercloud.com/avatars/rude_128.jpg",
+  location: "New Felixtown",
+};
+
+const mockUserResponseIdThree = {
+  id: "3",
+  firstName: "Marion",
+  lastName: "Kertzmann",
+  displayName: "marionKertzmann",
+  avatarUrl: "https://cdn.fakercloud.com/avatars/rude_128.jpg",
+  location: "Botsfordtown",
+};
+
+const mockUserResponseIdTen = {
+  id: "10",
+  firstName: "Travon",
+  lastName: "Nikolaus",
+  displayName: "travonNikolaus",
+  avatarUrl: "https://cdn.fakercloud.com/avatars/rude_128.jpg",
+  location: "Port Breanna",
+};
+
+const mockTeamResponseIdOne = {
+  id: "1",
+  name: "Ordinary Coral Lynx",
+  teamLeadId: 10,
+  teamMemberIds: [1, 2, 3],
 };
 
 export const handlers = [
-  rest.get(mockEndpoint + "/user", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockUserResponse));
+  rest.get(mockBaseUrl + "/users/1", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockUserResponseIdOne));
   }),
 
-  rest.get(mockEndpoint + "/teams", (req, res, ctx) => {
+  rest.get(mockBaseUrl + "/users/2", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockUserResponseIdTwo));
+  }),
+
+  rest.get(mockBaseUrl + "/users/3", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockUserResponseIdThree));
+  }),
+
+  rest.get(mockBaseUrl + "/users/10", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockUserResponseIdTen));
+  }),
+
+  rest.get(mockBaseUrl + "/teams", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(mockTeamsReponse));
+  }),
+
+  rest.get(mockBaseUrl + "/teams/*", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(mockTeamResponseIdOne));
   }),
 ];
